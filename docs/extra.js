4a65d184e977c250447b413d09ee4195135c3630
Eric Larson <ewlarson@gmail.com>
2022-09-19 09:46:59 -0500

    Google Analytics: add Google Tag
    First attempt.

---
+++ docs/extra.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-RFL265H4YX"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-RFL265H4YX');
+</script>
