5e9b67417558c89a9100a664750b00e94ff13192
Eric Larson <ewlarson@gmail.com>
2022-09-19 09:54:43 -0500

    Google Analytics: trying the config option

--- docs/extra.js
+++
@@ -1,9 +0,0 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-RFL265H4YX"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-RFL265H4YX');
-</script>
